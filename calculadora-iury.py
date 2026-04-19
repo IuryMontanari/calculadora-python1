509dc639ffaf4c5532ffcf75827c7aad2dce82b6
IuryMontanari <iurymontanari690@gmail.com>
2026-04-18 22:27:28 -0300

    Primeiro commit

---
+++ calculadora-iury.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+print("Escolha uma operação")
+print('m = multiplicação')
+print('d = divisão')
+print('a = adição')
+print('s = subtração')
+
+escolha = input('Digite uma das operações desejadas entre m,d,a,s: ')
+digite_um_numero = float(input("Digite um Número: "))
+digite_mais_um = float(input("Digite um Número: "))
+if escolha == 'm':
+  result=digite_um_numero*digite_mais_um
+  print(f'Resultado:',{result})
+elif escolha == 'd':
+  if digite_mais_um != 0:
+    result=digite_um_numero/digite_mais_um
+    print('Resultado',{result})
+  else:
+     print('Impossível dividir por zero')
+elif escolha == 'a':
+      result=digite_um_numero+digite_mais_um
+      print(f'Resultado',{result})
+elif escolha == 's':
+  result=digite_um_numero-digite_mais_um
+  print(f'Resultado',{result})
+else:
+  print('Por favor escolha uma letra entre m,d,a,s')
+chmod +x calculadora-iury.py
+./calculadora-iury
